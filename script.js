9ebd3de79f5cddcd6cdc7c2890f4327815e639bc
NS-web-creator <42011257+NS-web-creator@users.noreply.github.com>
2020-04-22 15:01:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function get(name){
 //ShareList passbyvalues Week 14
 function passlist()
 {
- var url = "https://rvclist.github.io/rvclist14/index.html?list="+ shoppinglist;
+ var url = "https://shulganlist.github.io/ShoppingList/index.html?list="+ shoppinglist;
  //Week 14 add link to sharelist id
       document.getElementById("sharelist").innerHTML = 'Share List:\n' + url;
  //Copy URL
